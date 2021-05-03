439d083208ba179d1b43fcaeae4be583f89a5898
Ziad Hegazi <ziadhegazi@hotmail.com>
2021-05-04 00:48:29 +0200

    Props Checkpoint

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import logo from "./ZH style 2 WIP3pdf.jpg"
 import './App.css';
 
 function App() {
+  const handleName = () => {
+    alert(`Hello, Ziad Hegazi`);
+  }
   return (
     <>
       <Profile 
       fullname={"Ziad Hegazi"} 
       bio={"I am fluent in English and an easy going person with great communication skills. I also am great with computers and technology and fixing problems. I self-teach myself some new skills frequently like new programming languages, and skills to use. Well motivated and self-disciplined driven by the motive of always improving myself constantly. "} 
-      profession={"Full Stack Developer"}>
+      profession={"Full Stack Developer"}
+      alertName={handleName}>
         <img src={logo} alt="Logo" className="logo"></img>
       </Profile>
       <div className="background"></div>
-      <div className="background2"></div>
     </>
   );
 }
--- src/Profile/profile.js
+++ src/Profile/profile.js
@@ -1,16 +1,31 @@
 
+import PropTypes from "prop-types";
+
 const profile = (props) => {
     const name = {margin:"1em auto 0.2em", fontWeight: 700, letterSpacing: "5px"};
     const ttl = {margin:"0 auto 1em", fontWeight: 300};
     const txt = {padding: "2px 15px", fontWeight: 100}
+    const fullname = props.fullname
     return (
-        <div style={{margin:"1em", textAlign:"center", color:"#fff"}}>
+        <div style={{margin:"1em", textAlign:"center", color:"#fff"}} onLoad={props.alertName}>
             {props.children}
-            <h2 style={name}>{props.fullname}</h2>
+            <h2 style={name}>{fullname}</h2>
             <h3 style={ttl}>{props.profession}</h3>
             <p style={txt}>{props.bio}</p>
         </div>
     );
+}
+
+
+profile.defaultProps = {
+    fullname: "stranger",
+    bio:"N/A",
+    profession:"N/A"
+}
+profile.propTypes = {
+    name:PropTypes.string,
+    bio:PropTypes.string,
+    profession:PropTypes.string
 }
 
 export default profile;
